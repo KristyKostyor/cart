65efd591ed8b15464da7750ca4d543fbb18c173f
KristyKostyor <ms.kostyor@gmail.com>
2023-05-04 11:31:49 +0300

    discount

--- cart.js
+++ cart.js
@@ -3,14 +3,17 @@
 const cart = {
   items: [],
   count: 0,
+  discount: 0,
 
   get totalPrice() {
     return this.calculateItemPrice();
   },
 
   calculateItemPrice() {
-    return this.items.reduce(
-        (acc, item) => acc + item.price * item.quantity, 0);
+    const discountedPrice =
+      this.items.reduce((acc, item) => acc + item.price * item.quantity, 0) *
+      (1 - this.discount / 100);
+    return `${discountedPrice.toFixed(2)} руб.`;
   },
 
   increaseCount(num) {
@@ -27,9 +30,20 @@ const cart = {
     this.increaseCount(quantity);
   },
 
+  set setDiscount(promocode) {
+    if (promocode === 'METHED') {
+      this.discount += 15;
+    } else if (promocode === 'NEWYEAR') {
+      this.discount += 21;
+    } else {
+      console.log('Invalid promocode');
+    }
+  },
+
   clear() {
     this.items = [];
     this.count = 0;
+    this.discount = 0;
   },
 
   print() {
@@ -42,3 +56,11 @@ cart.add('Item 1', 10, 2);
 cart.add('Item 2', 15);
 cart.add('Item 3', 20, 3);
 cart.print();
+
+cart.setDiscount = 'METHED';
+console.log(cart.discount); // 15
+console.log(cart.totalPrice); // 57.80 руб.
+
+cart.setDiscount = 'NEWYEAR';
+console.log(cart.discount); // 36
+console.log(cart.totalPrice); // 50.40 руб.
